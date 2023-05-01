58387480662831fc3b65cde2ffd582e6d7467c72
Andrej Finki <andrej.shekerov@students.finki.ukim.mk>
2023-05-01 16:44:34 +0200

    Add utility functions to vec3

--- src/vec3.cpp
+++ src/vec3.cpp
@@ -1,8 +1,7 @@
 #ifndef VEC3_H
 #define VEC3_H
 
-#include <cmath>
-#include <iostream>
+#include <bits/stdc++.h>
 
 class vec3 { public:
 
@@ -44,11 +43,50 @@ class vec3 { public:
 	double length_squared() const {
 		return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
 	}
-
 	
 };
 
 using point3 = vec3;
 using color = vec3;
+
+inline std::ostream &operator<<( std::ostream &out, const vec3 &v ) {
+	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+( const vec3 &u, const vec3 &v ) {
+	return vec3( u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2] );
+}
+
+inline vec3 operator-( const vec3 &u, const vec3 &v ) {
+	return vec3( u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2] );
+}
+
+inline vec3 operator*( const vec3 &u, const vec3 &v ) {
+	return vec3( u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2] );
+}
+
+inline vec3 operator*( double t, const vec3 &v ) {
+	return vec3( t*v.e[0], t*v.e[1], t*v.e[2] );
+}
+
+inline vec3 operator*( const vec3 &v, double t ) {
+	return t * v;
+}
+
+inline vec3 operator/( vec3 v, double t ) {
+	return (1/t) * v;
+}
+
+inline double dot( const vec3 &u, const vec3 &v ) {
+	return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+inline vec3 cross( const vec3 &u, const vec3 &v ) {
+	return vec3( u.e[1] * v.e[2] - u.e[2] * v.e[1], u.e[2] * v.e[0] - u.e[0] * v.e[2], u.e[0] * v.e[1] - u.e[1] * v.e[0] );
+}
+
+inline vec3 unit_vector(vec3 v) {
+	return v / v.length();
+}
 
 #endif
