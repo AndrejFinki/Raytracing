606058140f4eff804e42e98815f7ddbb33571fa0
Andrej Finki <andrej.shekerov@students.finki.ukim.mk>
2023-05-03 18:04:42 +0200

    Fix color.h to work with any value for image_max_color

--- src/color.h
+++ src/color.h
@@ -5,11 +5,18 @@
 
 #include <iostream>
 
+double MAX_COLOR = 256;
+const double ERR = 1e-5;
+
 void write_color(std::ostream &out, color pixel_color) {
     // Write the translated [0,255] value of each color component.
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-        << static_cast<int>(255.999 * pixel_color.y()) << ' '
-        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    out << static_cast<int>( MAX_COLOR * pixel_color.x() - ERR ) << ' '
+        << static_cast<int>( MAX_COLOR * pixel_color.y() - ERR ) << ' '
+        << static_cast<int>( MAX_COLOR * pixel_color.z() - ERR ) << '\n';
+}
+
+void set_max_color( double new_color ){
+	MAX_COLOR = new_color;
 }
 
 #endif
--- src/ppm.h
+++ src/ppm.h
@@ -1,10 +1,13 @@
 #ifndef PPM_H
 #define PPM_H
 
+#include "color.h"
+
 class PPM { public:
 
 	static void initialize_ppm( const std::string &ppm_type, const int &image_width, const int &image_height, const int &image_max_color ){
 		std::cout << ppm_type << "\n" << image_width << " " << image_height << "\n" << image_max_color << "\n";
+		set_max_color( image_max_color );
 	}
 
 };
