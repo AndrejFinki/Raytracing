2cc749777ff5bc42367563f2c7a8341dc74f3f47
finki <andrej.shekerov@students.finki.ukim.mk>
2023-06-27 17:00:03 +0200

    7.2 - Generating Pixels with Multiple Samples

--- src/GenerateImage.cpp
+++ src/GenerateImage.cpp
@@ -6,11 +6,13 @@
 #include "vec3.h"
 #include "color.h"
 #include "ray.h"
+#include "camera.h"
 
 const double ASPECT_RATIO = 16.0/9.0;
-const int IMAGE_WIDTH = 1920;
+const int IMAGE_WIDTH = 800;
 const int IMAGE_HEIGHT = int( IMAGE_WIDTH / ASPECT_RATIO );
 const int IMAGE_MAX_VALUE = 255;
+const int SAMPLES_PER_PIXEL = 25;
 
 color ray_color( const ray &, const hittable & );
 int main(){
@@ -21,25 +23,19 @@ int main(){
 	world.add( make_shared<sphere>( point3( 0, 0, -1 ), 0.5 ) );
 	world.add( make_shared<sphere>( point3( 0, -100.5, -1 ), 100 ) );
 
-	double viewport_height = 2;
-	double viewport_width = viewport_height * ASPECT_RATIO;
-	double focal_length = 1;
-
-	point3 origin = point3( 0, 0, 0 );
-	vec3 horizontal = vec3( viewport_width, 0, 0 );
-	vec3 vertical = vec3( 0, viewport_height, 0 );
-	vec3 lower_left_corner = origin - horizontal/2 - vertical/2 - vec3( 0, 0, focal_length );
+	camera cam;
 
 	for( int j = IMAGE_HEIGHT-1 ; j >= 0 ; j-- ) for( int i = 0 ; i < IMAGE_WIDTH ; i++ ){
 
-		double u = double(i) / ( IMAGE_WIDTH-1 );
-		double v = double(j) / ( IMAGE_HEIGHT-1 );
-
-		ray r( origin, lower_left_corner + u*horizontal + v*vertical - origin );
-
-		color pixel_color = ray_color( r, world );
-
-		write_color( std::cout, pixel_color );
+		color pixel_color( 0, 0, 0 );
+		for( int s = 0 ; s < SAMPLES_PER_PIXEL ; s++ ) {
+			double u = ( i + random_double() ) / ( IMAGE_WIDTH-1 );
+			double v = ( j + random_double() ) / ( IMAGE_HEIGHT-1 );
+			ray r = cam.get_ray( u, v );
+			pixel_color += ray_color( r, world );
+		}
+		
+		write_color( std::cout, pixel_color, SAMPLES_PER_PIXEL );
 
 	} 
 
